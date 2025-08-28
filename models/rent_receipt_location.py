e68e56a35a09fd41bf0caabbca233bca3dbaae6c
Yann Le Doaré <yann@linuxconsole.org>
2025-08-28 10:50:11 +0200

    Send mail button

--- models/rent_receipt_location.py
+++ models/rent_receipt_location.py
@@ -1,16 +1,52 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.http import Response, request
 
+from io import BytesIO
+import base64
+import io
+from PyPDF2 import PdfFileReader, PdfFileWriter
 
 class RentReceiptLocation(models.Model):
     _name = 'rent.receipt.location'
     _inherit = [
-        'mail.thread',
-        'mail.activity.mixin',
-        'image.mixin',
-    ]
+            'mail.thread',
+            'mail.activity.mixin',
+            'image.mixin',
+            ]
     _description = 'Rent receipt location'
+
+
+    def send_email_with_pdf_attach(self):
+      report_pdf = request.env[ "ir.actions.report" ]._render_qweb_pdf( "rent_receipt.report_rent_receipt_location", [self.id])
+      pdf_base64 = base64.b64encode(report_pdf[0])
+      return True
+      # TO be continued
+      attachment_values = {
+        'name': "THEFILE.pdf",
+        'type': 'binary',
+        'datas': pdf_base64,
+        'mimetype': 'application/pdf',
+      }
+      attachment = self.env['ir.attachment'].create(attachment_values)
+      ir_values = {
+            'name': 'Invoice Report',
+            'type': 'binary',
+            'datas': data_record,
+            'store_fname': data_record,
+            'mimetype': 'application/vnd.ms-excel',
+            'res_model': 'rent.receipt',
+            }
+      email_template = self.env.ref('module_name.email_template')
+      if email_template:
+            email_values = {
+                'email_to': self.partner_id.email,
+                'email_from': self.env.user.email,
+                }
+            email_template.attachment_ids = [(4, attachment.id)]
+            email_template.send_mail(self.id, email_values=email_values)
+            email_template.attachment_ids = [(5, 0, 0)]
 
     #name = fields.Char('Name')
     property_id = fields.Many2one('rent.receipt.property', string='Property')
@@ -36,26 +72,26 @@ class RentReceiptLocation(models.Model):
             record.amount_net = record.amount_charges + record.amount
 
     name_of_customer = fields.Char(
-        string='Customer Name',
-        related='customer_id.name')
+            string='Customer Name',
+            related='customer_id.name')
     zip_of_property = fields.Char(
-        string='Zip',
-        related='property_id.zip')
+            string='Zip',
+            related='property_id.zip')
     street_of_property = fields.Char(
-        string='Street',
-        related='property_id.street')
+            string='Street',
+            related='property_id.street')
     city_of_property = fields.Char(
-        string='City',
-        related='property_id.city')
+            string='City',
+            related='property_id.city')
     name_of_owner = fields.Char(
-        string='Owner Name',
-        related='property_id.owner_id.name')
+            string='Owner Name',
+            related='property_id.owner_id.name')
     zip_of_owner = fields.Char(
-        string='Owner Zip',
-        related='property_id.owner_id.zip')
+            string='Owner Zip',
+            related='property_id.owner_id.zip')
     street_of_owner = fields.Char(
-        string='Owner Street',
-        related='property_id.owner_id.street')
+            string='Owner Street',
+            related='property_id.owner_id.street')
     city_of_owner = fields.Char(
-        string='Owner City',
-        related='property_id.owner_id.city')
+            string='Owner City',
+            related='property_id.owner_id.city')
